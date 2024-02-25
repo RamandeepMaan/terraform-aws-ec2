165815b49a828a9b5f34072c76ba94474af38415
RamandeepMaan <119065521+RamandeepMaan@users.noreply.github.com>
2024-02-25 09:41:57 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,9 +1,4 @@
-terraform {
-  required_version = "~>1.6.4" // terraform binary version which can execute these tf files.  
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "5.29.0"
-    }
-  }
+provider "aws" { 
+
+    region = "ap-south-1"
 }
