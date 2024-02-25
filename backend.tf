a9ed82699fbbde83b681c642383ba30288b0364c
RamandeepMaan <119065521+RamandeepMaan@users.noreply.github.com>
2024-02-25 22:55:32 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 backend "s3" {
     bucket         	   = "dhuri"
-    key              	   = "state/terraform.tfstate"
+    key              	   = "terraform.tfstate"
     region         	   = "ap-south-1"
     
   }
