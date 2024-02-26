a95f161f7c9fa27a87f1bc1a39a9c61d2ca97480
RamandeepMaan <119065521+RamandeepMaan@users.noreply.github.com>
2024-02-27 00:47:59 +0530

    Update version.tf

--- version.tf
+++ version.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = "~>1.6.4" // terraform binary version which can execute these tf files.  
+  required_version = "~>1.7.4" // terraform binary version which can execute these tf files.  
   required_providers {
     aws = {
       source = "hashicorp/aws"
